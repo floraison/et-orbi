6773b3b38ef90bc109fdab98967f7d492493d823
John Mettraux <jmettraux@gmail.com>
2017-06-22 14:03:54 +0900

    Reorganize module methods

--- lib/et-orbi.rb
+++ lib/et-orbi.rb
@@ -12,134 +12,134 @@ module EtOrbi
   #
   # module methods
 
-  def self.now(zone=nil)
+  class << self
 
-    EoTime.new(Time.now.to_f, zone)
-  end
+    def now(zone=nil)
 
-  def self.parse(str, opts={})
-
-    if defined?(::Chronic) && t = ::Chronic.parse(str, opts)
-      return EoTime.new(t, nil)
+      EoTime.new(Time.now.to_f, zone)
     end
 
-    #rold = RUBY_VERSION < '1.9.0'
-    #rold = RUBY_VERSION < '2.0.0'
+    def parse(str, opts={})
+
+      if defined?(::Chronic) && t = ::Chronic.parse(str, opts)
+        return EoTime.new(t, nil)
+      end
+
+      #rold = RUBY_VERSION < '1.9.0'
+      #rold = RUBY_VERSION < '2.0.0'
 
 #p [ '---', str ]
-    begin
-      DateTime.parse(str)
-    rescue
-      fail ArgumentError, "no time information in #{str.inspect}"
-    end #if rold
-      #
-      # is necessary since Time.parse('xxx') in Ruby < 1.9 yields `now`
-
-    zone = izone = get_tzone(list_iso8601_zones(str).last)
-
-    list_olson_zones(str).each { |s| break if zone; zone = get_tzone(s) }
-
-    zone ||= local_tzone
-
-    str = str.sub(zone.name, '') unless zone.name.match(/\A[-+]/)
-      #
-      # for 'Sun Nov 18 16:01:00 Asia/Singapore 2012',
-      # although where does rufus-scheduler have it from?
-
-    local = Time.parse(str)
-
-    secs =
-      if izone
-        local.to_f
-      else
-        zone.period_for_local(local).to_utc(local).to_f
-      end
-
-    EoTime.new(secs, zone)
-  end
-
-  def self.make_time(*a)
-
-    o = a.length > 1 ? a : a.first
-
-    ot =
-      case o
-      when Array
-        time_to_eo_time(Time.local(*o))
-      when Time
-        time_to_eo_time(o)
-      when Date
-        time_to_eo_time(
-          o.respond_to?(:to_time) ?
-          o.to_time :
-          Time.parse(o.strftime('%Y-%m-%d %H:%M:%S')))
-      when String
-        parse(o)
-      else
-        o
-      end
-
-    ot = EoTime.new(Time.now.to_f + ot, nil) if ot.is_a?(Numeric)
-
-    fail ArgumentError.new(
-      "cannot turn #{o.inspect} to a EoTime instance"
-    ) unless ot.is_a?(EoTime)
-
-    ot
-  end
-
-  def self.get_tzone(o)
-
-    return o if o.is_a?(::TZInfo::Timezone)
-    return nil if o == nil
-    return local_tzone if o == :local
-    return ::TZInfo::Timezone.get('Zulu') if o == 'Z'
-
-    o = to_offset(o) if o.is_a?(Numeric)
-
-    return nil unless o.is_a?(String)
-
-    (@custom_tz_cache ||= {})[o] ||
-    get_offset_tzone(o) ||
-    (::TZInfo::Timezone.get(o) rescue nil)
-  end
-
-  def self.local_tzone
-
-    @local_tzone = nil \
-      if @local_tzone_loaded_at && (Time.now > @local_tzone_loaded_at + 1800)
-    @local_tzone = nil \
-      if @local_tzone_tz != ENV['TZ']
-
-    @local_tzone ||=
       begin
-        @local_tzone_tz = ENV['TZ']
-        @local_tzone_loaded_at = Time.now
-        determine_local_tzone
-      end
-  end
+        DateTime.parse(str)
+      rescue
+        fail ArgumentError, "no time information in #{str.inspect}"
+      end #if rold
+        #
+        # is necessary since Time.parse('xxx') in Ruby < 1.9 yields `now`
 
-  def self.platform_info
+      zone = izone = get_tzone(list_iso8601_zones(str).last)
 
-    etos = Proc.new { |k, v| "#{k}:#{v.inspect}" }
+      list_olson_zones(str).each { |s| break if zone; zone = get_tzone(s) }
 
-    '(' +
-      {
-        'etz' => ENV['TZ'],
-        'tnz' => Time.now.zone,
-        'tzid' => defined?(TZInfo::Data),
-        'rv' => RUBY_VERSION,
-        'rp' => RUBY_PLATFORM,
-        'eov' => EtOrbi::VERSION,
-        'rorv' => (Rails::VERSION::STRING rescue nil),
-        'astz' => Time.respond_to?(:zone) ? Time.zone.name : nil,
-          # Active Support Time.zone
-      }.collect(&etos).join(',') + ',' +
-      gather_tzs.collect(&etos).join(',') +
-    ')'
-  end
+      zone ||= local_tzone
 
-  class << self
+      str = str.sub(zone.name, '') unless zone.name.match(/\A[-+]/)
+        #
+        # for 'Sun Nov 18 16:01:00 Asia/Singapore 2012',
+        # although where does rufus-scheduler have it from?
+
+      local = Time.parse(str)
+
+      secs =
+        if izone
+          local.to_f
+        else
+          zone.period_for_local(local).to_utc(local).to_f
+        end
+
+      EoTime.new(secs, zone)
+    end
+
+    def make_time(*a)
+
+      o = a.length > 1 ? a : a.first
+
+      ot =
+        case o
+        when Array
+          time_to_eo_time(Time.local(*o))
+        when Time
+          time_to_eo_time(o)
+        when Date
+          time_to_eo_time(
+            o.respond_to?(:to_time) ?
+            o.to_time :
+            Time.parse(o.strftime('%Y-%m-%d %H:%M:%S')))
+        when String
+          parse(o)
+        else
+          o
+        end
+
+      ot = EoTime.new(Time.now.to_f + ot, nil) if ot.is_a?(Numeric)
+
+      fail ArgumentError.new(
+        "cannot turn #{o.inspect} to a EoTime instance"
+      ) unless ot.is_a?(EoTime)
+
+      ot
+    end
+
+    def get_tzone(o)
+
+      return o if o.is_a?(::TZInfo::Timezone)
+      return nil if o == nil
+      return local_tzone if o == :local
+      return ::TZInfo::Timezone.get('Zulu') if o == 'Z'
+
+      o = to_offset(o) if o.is_a?(Numeric)
+
+      return nil unless o.is_a?(String)
+
+      (@custom_tz_cache ||= {})[o] ||
+      get_offset_tzone(o) ||
+      (::TZInfo::Timezone.get(o) rescue nil)
+    end
+
+    def local_tzone
+
+      @local_tzone = nil \
+        if @local_tzone_loaded_at && (Time.now > @local_tzone_loaded_at + 1800)
+      @local_tzone = nil \
+        if @local_tzone_tz != ENV['TZ']
+
+      @local_tzone ||=
+        begin
+          @local_tzone_tz = ENV['TZ']
+          @local_tzone_loaded_at = Time.now
+          determine_local_tzone
+        end
+    end
+
+    def platform_info
+
+      etos = Proc.new { |k, v| "#{k}:#{v.inspect}" }
+
+      '(' +
+        {
+          'etz' => ENV['TZ'],
+          'tnz' => Time.now.zone,
+          'tzid' => defined?(TZInfo::Data),
+          'rv' => RUBY_VERSION,
+          'rp' => RUBY_PLATFORM,
+          'eov' => EtOrbi::VERSION,
+          'rorv' => (Rails::VERSION::STRING rescue nil),
+          'astz' => Time.respond_to?(:zone) ? Time.zone.name : nil,
+            # Active Support Time.zone
+        }.collect(&etos).join(',') + ',' +
+        gather_tzs.collect(&etos).join(',') +
+      ')'
+    end
 
     alias make make_time
   end
@@ -458,126 +458,176 @@ module EtOrbi
     end
   end
 
-  #
-  # not so public module methods
+  class << self
 
-  def self.time_to_eo_time(t)
+    #
+    # extra public methods
 
-    z =
-      get_tzone(t.zone) ||
-      (
-        local_tzone.period_for_local(t).abbreviation.to_s == t.zone &&
-        local_tzone
-      ) ||
-      t.zone
+    # https://en.wikipedia.org/wiki/ISO_8601
+    # Postel's law applies
+    #
+    def list_iso8601_zones(s)
 
-    EoTime.new(t.to_f, z)
-  end
-
-  def self.to_offset(n)
-
-    i = n.to_i
-    sn = i < 0 ? '-' : '+'; i = i.abs
-    hr = i / 3600; mn = i % 3600; sc = i % 60
-    (sc > 0 ? "%s%02d:%02d:%02d" : "%s%02d:%02d") % [ sn, hr, mn, sc ]
-  end
-
-  def self.get_offset_tzone(str)
-
-    # custom timezones, no DST, just an offset, like "+08:00" or "-01:30"
-
-    m = str.match(/\A([+-][0-1][0-9]):?([0-5][0-9])?\z/)
-    return nil unless m
-
-    hr = m[1].to_i
-    mn = m[2].to_i
-
-    hr = nil if hr.abs > 11
-    hr = nil if mn > 59
-    mn = -mn if hr && hr < 0
-
-    return (
-      @custom_tz_cache[str] =
-        begin
-          tzi = TZInfo::TransitionDataTimezoneInfo.new(str)
-          tzi.offset(str, hr * 3600 + mn * 60, 0, str)
-          tzi.create_timezone
-        end
-    ) if hr
-
-    nil
-  end
-
-  def self.determine_local_tzone
-
-    etz = ENV['TZ']
-
-    tz = ::TZInfo::Timezone.get(etz) rescue nil
-    return tz if tz
-
-    tz = Time.zone.tzinfo \
-      if Time.respond_to?(:zone) && Time.zone.respond_to?(:tzinfo)
-    return tz if tz
-
-    tzs = determine_local_tzones
-
-    (etz && tzs.find { |z| z.name == etz }) || tzs.first
-  end
-
-  def self.determine_local_tzones
-
-    tabbs = (-6..5)
-      .collect { |i| (Time.now + i * 30 * 24 * 3600).zone }
-      .uniq
-      .sort
-
-    t = Time.now
-    tu = t.dup.utc # /!\ dup is necessary, #utc modifies its target
-
-    twin = Time.utc(t.year, 1, 1) # winter
-    tsum = Time.utc(t.year, 7, 1) # summer
-
-    ::TZInfo::Timezone.all.select do |tz|
-
-      pabbs =
-        [
-          tz.period_for_utc(twin).abbreviation.to_s,
-          tz.period_for_utc(tsum).abbreviation.to_s
-        ].uniq.sort
-
-      pabbs == tabbs
+      s.scan(
+        %r{
+          (?<=:\d\d)
+          \s*
+          (?:
+            [-+]
+            (?:[0-1][0-9]|2[0-4])
+            (?:(?::)?(?:[0-5][0-9]|60))?
+            (?![-+])
+            |
+            Z
+          )
+        }x
+        ).collect(&:strip)
     end
-  end
 
-  # https://en.wikipedia.org/wiki/ISO_8601
-  # Postel's law applies
-  #
-  def self.list_iso8601_zones(s)
+    def list_olson_zones(s)
 
-    s.scan(
-      %r{
-        (?<=:\d\d)
-        \s*
-        (?:
-          [-+]
-          (?:[0-1][0-9]|2[0-4])
-          (?:(?::)?(?:[0-5][0-9]|60))?
-          (?![-+])
-          |
-          Z
-        )
-      }x
-      ).collect(&:strip)
-  end
+      s.scan(
+        %r{
+          (?<=\s|\A)
+          (?:[A-Za-z][A-Za-z0-9+_-]+)
+          (?:\/(?:[A-Za-z][A-Za-z0-9+_-]+)){0,2}
+        }x)
+    end
 
-  def self.list_olson_zones(s)
+    def determine_local_tzone
 
-    s.scan(
-      %r{
-        (?<=\s|\A)
-        (?:[A-Za-z][A-Za-z0-9+_-]+)
-        (?:\/(?:[A-Za-z][A-Za-z0-9+_-]+)){0,2}
-      }x)
+      etz = ENV['TZ']
+
+      tz = ::TZInfo::Timezone.get(etz) rescue nil
+      return tz if tz
+
+      tz = Time.zone.tzinfo \
+        if Time.respond_to?(:zone) && Time.zone.respond_to?(:tzinfo)
+      return tz if tz
+
+      tzs = determine_local_tzones
+
+      (etz && tzs.find { |z| z.name == etz }) || tzs.first
+    end
+
+    #
+    # protected module methods
+
+    protected
+
+    def time_to_eo_time(t)
+
+      z =
+        get_tzone(t.zone) ||
+        (
+          local_tzone.period_for_local(t).abbreviation.to_s == t.zone &&
+          local_tzone
+        ) ||
+        t.zone
+
+      EoTime.new(t.to_f, z)
+    end
+
+    def to_offset(n)
+
+      i = n.to_i
+      sn = i < 0 ? '-' : '+'; i = i.abs
+      hr = i / 3600; mn = i % 3600; sc = i % 60
+      (sc > 0 ? "%s%02d:%02d:%02d" : "%s%02d:%02d") % [ sn, hr, mn, sc ]
+    end
+
+    def get_offset_tzone(str)
+
+      # custom timezones, no DST, just an offset, like "+08:00" or "-01:30"
+
+      m = str.match(/\A([+-][0-1][0-9]):?([0-5][0-9])?\z/)
+      return nil unless m
+
+      hr = m[1].to_i
+      mn = m[2].to_i
+
+      hr = nil if hr.abs > 11
+      hr = nil if mn > 59
+      mn = -mn if hr && hr < 0
+
+      return (
+        @custom_tz_cache[str] =
+          begin
+            tzi = TZInfo::TransitionDataTimezoneInfo.new(str)
+            tzi.offset(str, hr * 3600 + mn * 60, 0, str)
+            tzi.create_timezone
+          end
+      ) if hr
+
+      nil
+    end
+
+    def determine_local_tzones
+
+      tabbs = (-6..5)
+        .collect { |i| (Time.now + i * 30 * 24 * 3600).zone }
+        .uniq
+        .sort
+
+      t = Time.now
+      tu = t.dup.utc # /!\ dup is necessary, #utc modifies its target
+
+      twin = Time.utc(t.year, 1, 1) # winter
+      tsum = Time.utc(t.year, 7, 1) # summer
+
+      ::TZInfo::Timezone.all.select do |tz|
+
+        pabbs =
+          [
+            tz.period_for_utc(twin).abbreviation.to_s,
+            tz.period_for_utc(tsum).abbreviation.to_s
+          ].uniq.sort
+
+        pabbs == tabbs
+      end
+    end
+
+    #
+    # system tz determination
+
+    def debian_tz
+
+      path = '/etc/timezone'
+
+      File.exist?(path) ? File.read(path).strip : nil
+    rescue; nil; end
+
+    def centos_tz
+
+      path = '/etc/sysconfig/clock'
+
+      File.open(path, 'rb') do |f|
+        until f.eof?
+          if m = f.readline.match(/ZONE="([^"]+)"/); return m[1]; end
+        end
+      end if File.exist?(path)
+
+      nil
+    rescue; nil; end
+
+    def osx_tz
+
+      path = '/etc/localtime'
+
+      File.symlink?(path) ?
+        File.readlink(path).split('/')[4..-1].join('/') :
+        nil
+    rescue; nil; end
+
+#    def find_tz
+#
+#      debian_tz || centos_tz || osx_tz
+#    end
+
+    def gather_tzs
+
+      { :debian => debian_tz, :centos => centos_tz, :osx => osx_tz }
+    end
   end
 
   #def in_zone(&block)
@@ -593,47 +643,5 @@ module EtOrbi
   #end
     #
     # kept around as a (thread-unsafe) relic
-
-  #
-  # system tz determination
-
-  def self.debian_tz
-
-    path = '/etc/timezone'
-
-    File.exist?(path) ? File.read(path).strip : nil
-  rescue; nil; end
-
-  def self.centos_tz
-
-    path = '/etc/sysconfig/clock'
-
-    File.open(path, 'rb') do |f|
-      until f.eof?
-        if m = f.readline.match(/ZONE="([^"]+)"/); return m[1]; end
-      end
-    end if File.exist?(path)
-
-    nil
-  rescue; nil; end
-
-  def self.osx_tz
-
-    path = '/etc/localtime'
-
-    File.symlink?(path) ?
-      File.readlink(path).split('/')[4..-1].join('/') :
-      nil
-  rescue; nil; end
-
-#  def self.find_tz
-#
-#    debian_tz || centos_tz || osx_tz
-#  end
-
-  def self.gather_tzs
-
-    { :debian => debian_tz, :centos => centos_tz, :osx => osx_tz }
-  end
 end
 
