76eee679ae64577b8baad8b092acd1059474d282
John Mettraux <jmettraux@gmail.com>
2026-07-07 19:33:33 +0900

    Remove debug output from eo_time_test.rb

--- test/eo_time_test.rb
+++ test/eo_time_test.rb
@@ -645,10 +645,7 @@ group EtOrbi::EoTime do
 
       assert t.to_s, '2007-10-31 19:25:00 -1100'
 
-      t1 = in_zone('Europe/Moscow') {
-        t.localtime
-.tap { |x| p [ x, x.to_s ] }
-      }
+      t1 = in_zone('Europe/Moscow') { t.localtime }
 
       assert t1.to_s, '2007-11-01 09:25:00 +0300'
       assert t1.object_id != t.object_id
